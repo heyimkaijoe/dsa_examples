916885e0caff960865b801cd5c29fc48e8ed979e
kchsu <kevin3117213@gmail.com>
2024-08-06 17:12:05 +0800

    Add DS queue from module queue's class SimpleQueue

--- data_structures/my_queue.py
+++ data_structures/my_queue.py
@@ -1,3 +1,4 @@
+# Approach 1
 from node import Node
 
 class Queue:
@@ -24,3 +25,16 @@ class Queue:
 
             if self.head is None:
                 self.tail = None
+
+# Approach 2
+import queue
+
+queue = queue.SimpleQueue()
+
+queue.put('apple')
+queue.put('pen')
+print(queue.qsize()) # 2
+
+print(queue.get())   # apple
+print(queue.get())   # pen
+print(queue.empty()) # True
