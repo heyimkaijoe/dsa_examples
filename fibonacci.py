23cd7e3179a853fb2e2bd28ebc44e8c9e4c0ce8c
kchsu <kevin3117213@gmail.com>
2024-08-08 21:32:34 +0800

    Modify: caching added to Fibonacci recursion to avoid recalculations

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,14 @@
+cache = [None]*100
+
 def fibonacci(n):
     if n < 0:
-        return 'Argument n should not be negative, please try again.'
+        return 'Argument should not be negative, please try again.'
     elif n <= 1:
         return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+    if not cache[n]:
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return cache[n]
 
 print(fibonacci(5))  # 5
 print(fibonacci(12)) # 144
