32679b2af1a75c8cbd7403aed3f71ed1d7b88814
kchsu <kevin3117213@gmail.com>
2024-08-12 03:40:24 +0800

    Add: second approach for Binary Search utilizing recursion

--- searching_algorithms/binary_search.py
+++ searching_algorithms/binary_search.py
@@ -1,4 +1,5 @@
 # Complexity: O(log n)
+# Approach 1
 def binary_search(ordered_list, search_value):
     start = 0
     end = len(ordered_list) - 1
@@ -14,5 +15,24 @@ def binary_search(ordered_list, search_value):
             end = middle - 1
     return False
 
+# Approach 2 w/ recursion
+def binary_search_recursion(ordered_list, search_value):
+    list_len = len(ordered_list)
+
+    if list_len == 0:
+        return False
+    else:
+        middle = (list_len) // 2
+        
+        if ordered_list[middle] == search_value:
+            return True
+        elif ordered_list[middle] < search_value:
+            return binary_search_recursion(ordered_list[middle+1:], search_value)
+        else:
+            return binary_search_recursion(ordered_list[:middle], search_value)
+
 print(binary_search([1,4,7,9,11,14], 4)) # True
 print(binary_search([1,4,7,9,11,14], 8)) # False
+
+print(binary_search_recursion([1,4,7,9,11,14], 4)) # True
+print(binary_search_recursion([1,4,7,9,11,14], 8)) # False
